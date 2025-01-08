08b6d5e2d3aad902b7ac2e4236ef2939aa7cbd25
saiki12 <saikiranhs338@gmail.com>
2025-01-08 17:52:53 -0600

    update URL in frontend to dynamically detect local and production environments

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -19,7 +19,7 @@ const HomePage = () => {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/analyze", {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/analyze`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
